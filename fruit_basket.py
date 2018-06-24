eac433ac19d8625ffd8125a63d3558dfc9ce8874
root <root@localhost.localdomain>
2018-06-24 00:19:02 -0500

    changes

---
+++ fruit_basket.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def fruit_basket():
+ fruit_basket =[apple, orange, banana, pear, plum]
+guessed_fruit = input ("Guess a fruit: ")
+if set('apple, orange, banana, pear, plum'):
+    print ("Correct!")
+else:
+    print ("Guess again")
